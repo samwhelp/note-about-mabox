d877af47088fa0ac1f97454edde0d5e04e0f448b
samwhelp <samwhelp@users.noreply.github.com>
2022-08-31 23:14:32 +0800

    fix

---
+++ _demo/prototype/adjust/asset/overlay/etc/skel/.config/tint2/profile/gruvbox-top-panel/start.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+
+THE_BASE_DIR_PATH="$(cd -- "$(dirname -- "$0")" ; pwd)"
+
+
+tint2 -c ${THE_BASE_DIR_PATH}/gruvbox-top.tint2rc &
+tint2 -c ${THE_BASE_DIR_PATH}/gruvbox-launcher.tint2rc &
