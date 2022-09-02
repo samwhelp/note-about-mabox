1f3335e1cce01dbbd327476c2aacc31116f27a1e
samwhelp <samwhelp@users.noreply.github.com>
2022-09-02 21:33:32 +0800

    fix

--- _demo/project/mabox-adjustment/tool/mabox-adjust-ctrl/gen/mabox-adjust-ctrl/Section/Model/fzf_install/Action.sh
+++ _demo/project/mabox-adjustment/tool/mabox-adjust-ctrl/gen/mabox-adjust-ctrl/Section/Model/fzf_install/Action.sh
@@ -1,6 +1,6 @@
 
 
 act_run_fzf_install () {
-	util_error_echo "act_run_fzf_install"
-	#mod_mabox_adjustment_module_install "$@"
+	#util_error_echo "act_run_fzf_install"
+	mod_mabox_adjustment_fzf_module_install "$@"
 }
--- _demo/project/mabox-adjustment/tool/mabox-adjust-ctrl/gen/mabox-adjust-ctrl/Section/Model/fzf_package_install/Action.sh
+++ _demo/project/mabox-adjustment/tool/mabox-adjust-ctrl/gen/mabox-adjust-ctrl/Section/Model/fzf_package_install/Action.sh
@@ -1,6 +1,6 @@
 
 
 act_run_fzf_package_install () {
-	util_error_echo "act_run_fzf_package_install"
-	#mod_mabox_adjustment_module_package_install "$@"
+	#util_error_echo "act_run_fzf_package_install"
+	mod_mabox_adjustment_fzf_module_package_install "$@"
 }
