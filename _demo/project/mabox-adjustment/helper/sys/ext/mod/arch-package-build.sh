2ef2847ae0dc0fc234f3b287921112e2c98ae353
samwhelp <samwhelp@users.noreply.github.com>
2022-09-03 21:30:10 +0800

    fix

--- _demo/project/mabox-adjustment/helper/sys/ext/mod/arch-package-build.sh
+++ _demo/project/mabox-adjustment/helper/sys/ext/mod/arch-package-build.sh
@@ -13,13 +13,97 @@ mod_arch_package_build () {
 	util_error_echo
 
 
-	local pkgbuild_dir_path="${THE_SHARE_DIR_PATH}/packaging/arch/mabox-adjustment-core"
+	sys_arch_package_build_mabox_adjustment_core
 
-
-	echo "${pkgbuild_dir_path}"
 
 }
 
 ##
 ### Tail: Mod / Arch / Package / Build
+################################################################################
+
+
+################################################################################
+### Head: Sys / Arch / Package / Build
+##
+
+sys_arch_package_build_mabox_adjustment_core () {
+
+
+	local pkgbuild_source_dir_path="${THE_SHARE_DIR_PATH}/packaging/arch/mabox-adjustment-core"
+
+	local pkgbuild_target_dir_path="${HOME}/.config/mabox-adjustment/var/mabox-adjustment-core"
+
+	#echo "${pkgbuild_source_dir_path}"
+	#echo "${pkgbuild_target_dir_path}"
+
+	util_error_echo "rm -rf ${pkgbuild_target_dir_path}"
+	rm -rf "${pkgbuild_target_dir_path}"
+
+	util_error_echo "mkdir -p ${pkgbuild_target_dir_path}/asset/overlay"
+	mkdir -p "${pkgbuild_target_dir_path}/asset/overlay"
+
+	util_error_echo "cp -rf ${pkgbuild_source_dir_path}/. ${pkgbuild_target_dir_path}"
+	cp -rf "${pkgbuild_source_dir_path}/." "${pkgbuild_target_dir_path}"
+
+
+	sys_arch_package_build_mabox_adjustment_core_copy_asset
+
+
+	util_error_echo "cd ${pkgbuild_target_dir_path}"
+	cd "${pkgbuild_target_dir_path}"
+
+	util_error_echo "./build.sh"
+	./build.sh
+
+	util_error_echo "cd ${OLDPWD}"
+	cd "${OLDPWD}"
+
+	##
+	## $ man cd | grep OLD
+	##
+
+	util_error_echo
+	util_error_echo '##'
+	util_error_echo '## New Packge Info:'
+	util_error_echo '##'
+	util_error_echo
+
+	util_error_echo
+	util_error_echo '##'
+	util_error_echo '## Package Dir: ~/.config/mabox-adjustment/var/mabox-adjustment-core/package/'
+	util_error_echo "## Package File: $(ls ~/.config/mabox-adjustment/var/mabox-adjustment-core/package)"
+	util_error_echo '##'
+	util_error_echo
+
+	util_error_echo
+	util_error_echo '## Please run:'
+	util_error_echo
+	util_error_echo 'cd ~/.config/mabox-adjustment/var/mabox-adjustment-core/'
+	util_error_echo
+	util_error_echo 'make reinstall'
+	util_error_echo
+
+}
+
+
+sys_arch_package_build_mabox_adjustment_core_copy_asset () {
+
+	local plan_source_dir_path="${THE_PLAN_DIR_PATH}"
+
+	local pkgbuild_target_dir_path="${HOME}/.config/mabox-adjustment/var/mabox-adjustment-core"
+
+	local plan_target_dir_path="$pkgbuild_target_dir_path/asset/overlay/usr/share/mabox-adjustment"
+
+	util_error_echo "mkdir -p ${plan_target_dir_path}"
+	mkdir -p "${plan_target_dir_path}"
+
+	util_error_echo "cp -rf ${plan_source_dir_path}/. ${plan_target_dir_path}"
+	cp -rf "${plan_source_dir_path}/." "${plan_target_dir_path}"
+
+}
+
+
+##
+### Tail: Sys / Arch / Package / Build
 ################################################################################
