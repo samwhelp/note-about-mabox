0cadcee48d7387306dd6456052413575ca377de4
samwhelp <samwhelp@users.noreply.github.com>
2022-09-03 20:12:26 +0800

    fix

--- _demo/project/mabox-adjustment/helper/sys/ext/init.sh
+++ _demo/project/mabox-adjustment/helper/sys/ext/init.sh
@@ -11,7 +11,7 @@
 source "${THE_SYS_EXT_MOD_DIR_PATH}/demo.sh"
 source "${THE_SYS_EXT_MOD_DIR_PATH}/package-list.sh"
 source "${THE_SYS_EXT_MOD_DIR_PATH}/package-install.sh"
-
+source "${THE_SYS_EXT_MOD_DIR_PATH}/arch-package-build.sh"
 
 ##
 ### Tail: Func / Init
---
+++ _demo/project/mabox-adjustment/helper/sys/ext/mod/arch-package-build.sh
@@ -0,0 +1,20 @@
+
+
+################################################################################
+### Head: Mod / Arch / Package / Build
+##
+
+mod_arch_package_build () {
+
+	util_error_echo
+	util_error_echo '##'
+	util_error_echo '## mod_arch_package_build'
+	util_error_echo '##'
+	util_error_echo
+
+
+}
+
+##
+### Tail: Mod / Arch / Package / Build
+################################################################################
