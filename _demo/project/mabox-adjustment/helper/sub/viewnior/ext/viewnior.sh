355c5e7732114cd0c8820902d9585b6132c94f95
samwhelp <samwhelp@users.noreply.github.com>
2022-09-04 15:24:53 +0800

    fix

--- _demo/project/mabox-adjustment/helper/sub/viewnior/ext/viewnior.sh
+++ _demo/project/mabox-adjustment/helper/sub/viewnior/ext/viewnior.sh
@@ -48,6 +48,14 @@ mod_viewnior_config_install () {
 	util_error_echo "install -Dm644 ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/viewnior/viewnior.conf ${HOME}/.config/viewnior/viewnior.conf"
 	install -Dm644 "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/viewnior/viewnior.conf" "${HOME}/.config/viewnior/viewnior.conf"
 
+
+	#util_error_echo "cp -rf ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/viewnior/. ${HOME}/.config/viewnior/"
+	#cp -rf "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/viewnior/." "${HOME}/.config/viewnior/"
+
+	#util_error_echo "cp -rfv ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/viewnior/. ${HOME}/.config/viewnior/"
+	#cp -rfv "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/viewnior/." "${HOME}/.config/viewnior/"
+
+
 }
 
 ##
