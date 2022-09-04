29ccffc486d895d6e48e77f534c44b0cf06e4933
samwhelp <samwhelp@users.noreply.github.com>
2022-09-04 15:12:41 +0800

    fix

--- _demo/project/mabox-adjustment/helper/sub/openbox/ext/openbox.sh
+++ _demo/project/mabox-adjustment/helper/sub/openbox/ext/openbox.sh
@@ -44,8 +44,26 @@ mod_openbox_config_install () {
 	util_error_echo "mkdir -p ${HOME}/.config/openbox"
 	mkdir -p "${HOME}/.config/openbox"
 
-	#util_error_echo "install -Dm644 ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/openbox.conf ${HOME}/.config/openbox/openbox.conf"
-	#install -Dm644 "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/openbox.conf" "${HOME}/.config/openbox/openbox.conf"
+	util_error_echo "install -Dm644 ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/rc.xml ${HOME}/.config/openbox/rc.xml"
+	install -Dm644 "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/rc.xml" "${HOME}/.config/openbox/rc.xml"
+
+	util_error_echo "install -Dm644 ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/menu.xml ${HOME}/.config/openbox/menu.xml"
+	install -Dm644 "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/menu.xml" "${HOME}/.config/openbox/menu.xml"
+
+	util_error_echo "install -Dm644 ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/environment ${HOME}/.config/openbox/environment"
+	install -Dm644 "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/environment" "${HOME}/.config/openbox/environment"
+
+	util_error_echo "install -Dm644 ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/autostart ${HOME}/.config/openbox/autostart"
+	install -Dm755 "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/autostart" "${HOME}/.config/openbox/autostart"
+
+
+
+	#util_error_echo "cp -rf ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/. ${HOME}/.config/openbox/"
+	#cp -rf "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/." "${HOME}/.config/openbox/"
+
+	#util_error_echo "cp -rfv ${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/. ${HOME}/.config/openbox/"
+	#cp -rfv "${THE_OVERLAY_DIR_PATH}/etc/skel/.config/openbox/." "${HOME}/.config/openbox/"
+
 
 }
 
